6e523a723112a202c5653e4aa64cde3bfb61d109
joshua-journey-apps <joshua@journeyapps.com>
2025-07-24 15:23:57 +0200

    implement sync demostration

--- App.tsx
+++ App.tsx
@@ -1,27 +1,269 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import '@azure/core-asynciterator-polyfill';
+import React, { useCallback, useEffect } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import {
+  useQuery,
+  PowerSyncContext,
+  usePowerSync,
+} from '@powersync/react-native';
+import { system, SystemContext, useOpSqlite } from './SystemContext';
+import { PlusIcon, XIcon } from 'lucide-react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+type Customer = {
+  id: string;
+  name: string;
+};
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(): React.JSX.Element {
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        await system.init();
+        console.log('System initialized successfully');
+      } catch (error) {
+        console.error('Error initializing system:', error);
+      }
+    };
+    initialize();
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <SystemContext.Provider value={system}>
+      <PowerSyncContext.Provider value={system.powersync}>
+        <SafeAreaView>
+          <Image source={require('./logo.png')} style={styles.logo} />
+          <View style={styles.mainContainer}>
+            <View>
+              <Text style={styles.sectionTitle}>Async List</Text>
+              <AsyncCustomerList />
+            </View>
+            <View>
+              <Text style={styles.sectionTitle}>Sync List</Text>
+              <SyncCustomerList />
+            </View>
+          </View>
+        </SafeAreaView>
+      </PowerSyncContext.Provider>
+    </SystemContext.Provider>
+  );
+}
+
+function AsyncCustomerList() {
+  const {
+    data: customers,
+    isLoading,
+    error,
+  } = useQuery<Customer>('SELECT * FROM customers');
+  const powerSync = usePowerSync();
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>Error: {error.message}</Text>;
+  }
+
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={async () => {
+          const id = Math.floor(Math.random() * 1000).toString();
+          await powerSync.execute(
+            'INSERT INTO customers(id, name) VALUES(?, ?)',
+            [id, `Customer ${id}`],
+          );
+        }}
+        style={styles.addButton}
+      >
+        <PlusIcon stroke={'#0894ff'} />
+        <Text style={styles.addButtonText}>Customer</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={customers}
+        renderItem={({ item: customer }) => (
+          <TouchableOpacity
+            key={customer.id}
+            onPress={async () => {
+              await powerSync.execute('DELETE FROM customers WHERE id = ?', [
+                customer.id,
+              ]);
+            }}
+            style={styles.customerItem}
+          >
+            <Text key={customer.id} style={styles.customerName}>
+              {customer.name}
+            </Text>
+            <XIcon stroke={'grey'} size={18} />
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
+
+function SyncCustomerList() {
+  const [customers, setCustomers] = React.useState<Customer[]>([]);
+  const opSqlite = useOpSqlite();
+
+  const getCustomers = useCallback(() => {
+    if (!opSqlite) return;
+
+    try {
+      const result = opSqlite.executeSync('SELECT * FROM customers');
+      setCustomers(
+        result.rows.map(row => ({
+          id: row.id as string,
+          name: row.name as string,
+        })),
+      );
+    } catch (error) {
+      console.error('Error fetching customers:', error);
+      return;
+    }
+  }, [opSqlite]);
+
+  // Process transactions from ps_crud
+  const flushCrudTransactions = async () => {
+    await system.connector.uploadData(system.powersync);
+  };
+
+  // Get initial customers when the component mounts
+  useEffect(() => {
+    getCustomers();
+  }, [opSqlite, getCustomers]);
+
+  // Listens for changes from PowerSync updates
+  useEffect(() => {
+    const disposeChange = system.powersync.onChangeWithCallback(
+      {
+        onChange: () => {
+          getCustomers();
+        },
+      },
+      {
+        tables: ['customers'],
+      },
+    );
+
+    return () => {
+      disposeChange();
+    };
+  }, [getCustomers]);
+
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          const id = Math.floor(Math.random() * 1000);
+          opSqlite.executeSync(
+            'INSERT INTO ps_crud (id, data, tx_id) VALUES(?, ?, ?)',
+            [
+              id,
+              JSON.stringify({
+                op: 'PUT',
+                type: 'customers',
+                id,
+                data: {
+                  name: `Customer ${id}`,
+                },
+              }),
+            ],
+          );
+          flushCrudTransactions();
+          getCustomers();
+        }}
+        style={styles.addButton}
+      >
+        <PlusIcon stroke={'#0894ff'} />
+        <Text style={styles.addButtonText}>Customer</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={customers}
+        renderItem={({ item: customer }) => (
+          <TouchableOpacity
+            key={customer.id}
+            onPress={() => {
+              const id = Math.floor(Math.random() * 1000);
+              // Add to the upload queue (ps_crud) for deletion
+              opSqlite.executeSync(
+                'INSERT INTO ps_crud (id, data, tx_id) VALUES(?, ?, ?)',
+                [
+                  id,
+                  JSON.stringify({
+                    op: 'DELETE',
+                    type: 'customers',
+                    id: customer.id,
+                  }),
+                  id,
+                ],
+              );
+              flushCrudTransactions();
+              getCustomers();
+            }}
+            style={styles.customerItem}
+          >
+            <Text key={customer.id} style={styles.customerName}>
+              {customer.name}
+            </Text>
+            <XIcon stroke={'grey'} size={18} />
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  logo: {
+    marginVertical: 20,
+    objectFit: 'contain',
+    height: 50,
+    width: '100%',
+  },
+  mainContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 25,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+  addButton: {
+    marginVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 4,
+  },
+  addButtonText: {
+    fontSize: 16,
+    color: '#0894ff',
+  },
+  customerItem: {
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 10,
+  },
+  customerName: {
+    fontSize: 16,
   },
 });
 
---
+++ SystemContext.tsx
@@ -0,0 +1,130 @@
+import { open } from '@op-engineering/op-sqlite';
+import { OPSqliteOpenFactory } from '@powersync/op-sqlite';
+import {
+  AbstractPowerSyncDatabase,
+  column,
+  createBaseLogger,
+  LogLevel,
+  PowerSyncDatabase,
+  Schema,
+  Table,
+} from '@powersync/react-native';
+import React from 'react';
+import { Platform } from 'react-native';
+
+const localhost = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+const Config = {
+    PS_BACKEND_URL: `http://${localhost}:6060`,
+    PS_POWERSYNC_URL: `http://${localhost}:8080`,
+}
+
+const logger = createBaseLogger();
+logger.useDefaults();
+logger.setLevel(LogLevel.DEBUG);
+
+export class SelfhostConnector {
+  private _clientId: string | null = null;
+
+  async fetchCredentials() {
+    const token = await fetch(`${Config.PS_BACKEND_URL}/api/auth/token`)
+      .then(response => response.json())
+      .then(data => data.token);
+
+    return {
+      endpoint: Config.PS_POWERSYNC_URL,
+      token,
+    };
+  }
+
+  async uploadData(database: AbstractPowerSyncDatabase): Promise<void> {
+    const transaction = await database.getNextCrudTransaction();
+    console.log('Uploading data for transaction:', transaction);
+
+    if (!transaction) {
+      return;
+    }
+
+    if (!this._clientId) {
+      this._clientId = await database.getClientId();
+    }
+
+    try {
+      let batch: any[] = [];
+      for (let operation of transaction.crud) {
+        let payload = {
+          op: operation.op,
+          table: operation.table,
+          id: operation.id,
+          data: operation.opData,
+        };
+        batch.push(payload);
+      }
+
+      const response = await fetch(`${Config.PS_BACKEND_URL}/api/data`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ batch }),
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `Received ${
+            response.status
+          } from /api/data: ${await response.text()}`,
+        );
+      }
+
+      await transaction
+        .complete
+        // import.meta.env.VITE_CHECKPOINT_MODE == CheckpointMode.CUSTOM
+        //   ? await this.getCheckpoint(this._clientId)
+        //   : undefined
+        ();
+      console.log('Transaction completed successfully');
+    } catch (ex: any) {
+      console.debug(ex);
+      throw ex;
+    }
+  }
+}
+
+const customers = new Table({ name: column.text });
+const schema = new Schema({ customers });
+export const DB_NAME = 'powersync-test.db';
+
+export class System {
+  connector: SelfhostConnector;
+  powersync: PowerSyncDatabase;
+
+  constructor() {
+    this.connector = new SelfhostConnector();
+    this.powersync = new PowerSyncDatabase({
+      schema,
+      database: new OPSqliteOpenFactory({
+        dbFilename: DB_NAME,
+      }),
+      logger,
+    });
+  }
+
+  async init() {
+    await this.powersync.init();
+    await this.powersync.connect(this.connector, {
+      // clientImplementation: SyncClientImplementation.RUST
+    });
+
+    await this.powersync.waitForFirstSync();
+  }
+}
+
+export const system = new System();
+export const SystemContext = React.createContext(system);
+export const useSystem = () => React.useContext(SystemContext);
+
+const opSqlite = open({
+  name: DB_NAME,
+});
+export const OpSqliteContext = React.createContext(opSqlite);
+export const useOpSqlite = () => React.useContext(OpSqliteContext);
